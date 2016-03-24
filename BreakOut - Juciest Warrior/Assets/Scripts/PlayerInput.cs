8e6ab802e3c893f350675ddad40da49f1ecd475a
TheInstantGamer <nkstevo@gmail.com>
2016-03-23 22:28:38 -0400

    Added sprites and began work on PlayerController
    Used public domain BreakOut sprites from :
    http://opengameart.org/content/breakout-set and made by Buch with
    contributions from Surt and InanZen. These sprites will be used for the
    entire project. Began working on a script which allows the player to
    move the paddle.

---
+++ BreakOut - Juciest Warrior/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+    public float moveSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (horizontal != 0)
+        {
+            transform.Translate(new Vector3(horizontal, 0, 0) * Time.deltaTime * moveSpeed);
+        }
+	}
+}
